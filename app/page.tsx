c076d224b2e7298396b4362a2fb7f2e4a9839bda
MFA <mfa@MFA-MacBook-Pro.local>
2025-08-13 12:31:11 +0300

    init: Next.js + Tailwind full setup

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            visual studio dan commiit ettik
           </li>
         </ol>
 
